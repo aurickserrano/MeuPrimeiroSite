02cf7f02aaddf91007f562230bda153b0a092690
Aurick <aurickserrano@hotmail.com>
2022-03-09 22:07:14 -0300

    master

---
+++ MeuPrimeiroSite/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using MeuPrimeiroSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MeuPrimeiroSite.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+            home.Email = "aurick serrano";
+            home.Email = "aurickserrano@hotmail.com";
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
